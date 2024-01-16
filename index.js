0b14cc7b1c20dbbbc923f5241c669a77f3a8f933
HiiragiNozomi <155616611+HiiragiNozomi@users.noreply.github.com>
2024-01-16 20:27:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+let a = "https://www.youtube.com/watch?v=T9R-NPhRi1M"
 let url = document.getElementById("video").url;
-ytdl(url).pipe(fs.createWriteStream(`${url}.mp4`))
+ytdl(a).pipe(fs.createWriteStream(`${url}.mp4`))
